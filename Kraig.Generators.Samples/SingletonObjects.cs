62cce5af8d2d8a3e8cc399c6265587b597e359e3
ornaras <ornaras.us@gmail.com>
2025-10-06 22:49:11 +0400

    Написан пример генератора SingletonGenerator

---
+++ Kraig.Generators.Samples/SingletonObjects.cs
@@ -0,0 +1,20 @@
+﻿using Kraig.Attributes;
+
+namespace Kraig.Generators.Samples.SingletonObjects
+{
+    [Singleton]
+    public partial class SimpleSingleton
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+    }
+
+    [Singleton]
+    public partial class SingletonWithConstructor
+    {
+        public Guid Id { get; init; }
+        private SingletonWithConstructor()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
