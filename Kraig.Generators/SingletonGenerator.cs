151402c79143adbdddf6f60f72ae2be3e9ef32ea
ornaras <ornaras.us@gmail.com>
2025-10-06 22:30:02 +0400

    Реализован генератор SingletonGenerator

---
+++ Kraig.Generators/SingletonGenerator.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+
+namespace Kraig.Generators
+{
+    [Generator(LanguageNames.CSharp)]
+    internal class SingletonGenerator : IIncrementalGenerator
+    {
+        private const string ATTRIBUTE_FULLNAME = "Kraig.Attributes.SingletonAttribute";
+
+        private const string ATTRIBUTE_CLASS = """
+            using System;
+                                         
+            namespace Kraig.Attributes 
+            {
+                [AttributeUsage(AttributeTargets.Class)]
+                public class SingletonAttribute : Attribute { }
+            }
+            """;
+
+        private const string TEMPLATE = """
+            namespace {0}
+            {
+                partial class {1}
+                {
+                    public static {1} Instance
+                    {
+                        get
+                        {
+                            if(_instance is null) 
+                                _instance = new {1}();
+                            return _instance;
+                        }
+                    }
+                    private static {1} _instance;{2}
+                }
+            }
+            """;
+
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(GenerateAttribute);
+            var classes = context.SyntaxProvider.CreateSyntaxProvider(
+                (node, _) => node is ClassDeclarationSyntax,
+                (ctx, _) => (ITypeSymbol)ctx.SemanticModel.GetDeclaredSymbol(ctx.Node));
+            context.RegisterSourceOutput(classes.Collect(), GenerateCode);
+        }
+
+        private static void GenerateAttribute(IncrementalGeneratorPostInitializationContext context) =>
+            context.AddSource("SingletonAttribute.g.cs", ATTRIBUTE_CLASS);
+
+        private static void GenerateCode(SourceProductionContext ctx, ImmutableArray<ITypeSymbol> arr)
+        {
+            var builder = new StringBuilder();
+
+            arr = [.. arr.Where(t => t.GetAttributes().Any(a => a.AttributeClass.ToDisplayString() == ATTRIBUTE_FULLNAME))];
+
+            foreach (var type in arr.Distinct<ITypeSymbol>(SymbolEqualityComparer.Default))
+            {
+                var ns = type.ContainingNamespace.ToDisplayString();
+                var constructor = "";
+                if (NeedAutoConstructor(type))
+                    constructor = $"\n\t\tprivate {type.Name}() {{ }}";
+                builder.AppendLine(TEMPLATE.Replace("{0}", ns)
+                    .Replace("{1}", type.Name).Replace("{2}", constructor));
+            }
+
+            ctx.AddSource("Singletons.g.cs", builder.ToString());
+        }
+
+        private static bool NeedAutoConstructor(ITypeSymbol type) => type is INamedTypeSymbol @class &&
+            @class.InstanceConstructors.All(c => c.DeclaredAccessibility != Accessibility.Private || c.Parameters.Length > 0);
+    }
+}
