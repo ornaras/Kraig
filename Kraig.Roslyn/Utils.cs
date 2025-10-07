657da158a23065426c14f8acd862c82b1c103c56
ornaras <ornaras.us@gmail.com>
2025-10-07 09:39:38 +0400

    Добавлена вспомогательный метод ToPascalCase

---
+++ Kraig.Roslyn/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Kraig.Roslyn
+{
+    internal static class Utils
+    {
+        public static string ToPascalCase(this string value)
+        {
+            var result = new StringBuilder();
+            for(var i = 0; i < value.Length; i++)
+            {
+                if (value[i] == '_')
+                    result.Append(char.ToUpper(value[++i]));
+                else result.Append(value[i]);
+            }
+            return result.ToString();
+        }
+    }
+}
