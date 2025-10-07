52e2d4086c5ac20f3b89d0106a8dff2e04bc12c4
ornaras <ornaras.us@gmail.com>
2025-10-07 13:25:51 +0400

    Обновление примерочного класса генератора NotifyChangedGenerator

--- Kraig.Roslyn.Samples/NotifyChangedObjects.cs
+++ Kraig.Roslyn.Samples/NotifyChangedObjects.cs
@@ -2,10 +2,20 @@
 
 namespace Kraig.Roslyn.Samples.NotifyChangedObjects
 {
-    internal partial class Class1
+    internal partial class DefaultClass
     {
         [NotifyChanged] private int _digit = 10;
 
         public int Sqrt() => (int)Math.Sqrt(Digit);
+    }
+
+    internal partial class ClassEvent
+    {
+        [NotifyChanged(GenerateEvent = true)] private int _digit = 15;
+
+        public ClassEvent()
+        {
+            DigitChanged += () => Console.Beep();
+        }
     }
 }
