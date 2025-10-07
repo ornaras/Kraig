f023d2423a8ae101df8e5eb85bb07ac6396fea80
ornaras <ornaras.us@gmail.com>
2025-10-07 09:41:41 +0400

    Добавлен пример для генератора NotifyChangedGenerator

---
+++ Kraig.Roslyn.Samples/NotifyChangedObjects.cs
@@ -0,0 +1,11 @@
+﻿using Kraig.Attributes;
+
+namespace Kraig.Roslyn.Samples.NotifyChangedObjects
+{
+    internal partial class Class1
+    {
+        [NotifyChanged] private int _digit = 10;
+
+        public int Sqrt() => (int)Math.Sqrt(Digit);
+    }
+}
